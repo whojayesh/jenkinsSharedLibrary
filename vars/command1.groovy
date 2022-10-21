8a41c1e177ed0bc5c303c89b2d2e1625df15c5a1
Jayesh Bhakat <52574996+whojayesh@users.noreply.github.com>
2022-10-21 10:52:49 +0530

    Update command1.groovy

--- vars/command1.groovy
+++ vars/command1.groovy
@@ -1,10 +1,12 @@
 def call(Closure closure){
   node('slave1'){
+    stage('library'){
       timestamps{
         closure()
       }
       git 'https://github.com/whojayesh/jenkinsSharedLibrary.git'
       echo '---------'
+    }
   }
   
 }
